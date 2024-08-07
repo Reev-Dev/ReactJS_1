80fc1851399515d10eafbfbaed193fecc9b89398
attrfai <attarrifaismktibazma@gmail.com>
2024-08-07 14:31:54 +0700

    Lat 3 - Template Literals

--- exercise.js
+++ exercise.js
@@ -11,3 +11,8 @@ const greet = (name) => {
     console.log(`Hello, ${name}!`);
 };
 greet("Rifai"); // Output: Hello, Rifai!
+
+// template literals
+const boy = "Bowo";
+const message = `Hey, ${boy}!`;
+console.log(message); // Output: Hey, Bowo!
