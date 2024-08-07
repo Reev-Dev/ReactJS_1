dd6c36138a21823ca8c383e0d2c3c54d9ae46d02
attrfai <attarrifaismktibazma@gmail.com>
2024-08-07 14:37:14 +0700

    Lat 4 - Destructuring Array & Object

--- exercise.js
+++ exercise.js
@@ -16,3 +16,13 @@ greet("Rifai"); // Output: Hello, Rifai!
 const boy = "Bowo";
 const message = `Hey, ${boy}!`;
 console.log(message); // Output: Hey, Bowo!
+
+// destructuring array
+const num = [10, 20, 30];
+const [ten, twenty, thirty] = num;
+console.log(ten, twenty, thirty); // Output: 10 20 30
+
+// destructuring object
+const person = { firstName: "Attar", lastName: "Rifai" };
+const { firstName, lastName } = person;
+console.log(firstName, lastName); // Output: Rifai 17
