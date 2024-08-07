1af1db107a9d7e2a623b6085916a3f3817572100
attrfai <attarrifaismktibazma@gmail.com>
2024-08-07 14:28:10 +0700

    Lat 2 - Arrow Function

--- ReactJS_1/exercise.js
+++ ReactJS_1/exercise.js
@@ -5,3 +5,9 @@ console.log(namae); // Output: Rifai
 // const
 const age = 17;
 console.log(age); // Output: 17
+
+// arrow function
+const greet = (name) => {
+    console.log(`Hello, ${name}!`);
+};
+greet("Rifai"); // Output: Hello, Rifai!
